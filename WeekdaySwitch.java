7eb3b009b6f39b0d60f70d53dab95794e01e8867
B Neha Reddy <nehareddy1421@gmail.com>
2024-12-16 22:41:22 +0530

    Add files via upload

---
+++ WeekdaySwitch.java
@@ -0,0 +1,42 @@
+//java Program that keeps a number from the user and generates an integer between 1 and 7 
+//and displays the name of the weekday.
+
+
+import java.util.Scanner;
+
+public class WeekdaySwitch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input a number from the user
+        System.out.print("Enter a number between 1 and 7: ");
+        int day = scanner.nextInt();
+
+        // Determine and display the weekday using switch-case
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid input! Please enter a number between 1 and 7.");
+        }
+    }
+}
